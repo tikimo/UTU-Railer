69fb21f0289c56b5db2245ce9bd1a0b17023137e
Tijam Moradi <tikimo@utu.fi>
2018-01-25 15:34:16 +0200

    Login window is final, needs only logo

--- src/FrontEnd/Login/LoginController.java
+++ src/FrontEnd/Login/LoginController.java
@@ -1,9 +1,12 @@
 package FrontEnd.Login;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class LoginController {
     public Label createAccountLink;
@@ -11,8 +14,25 @@ public class LoginController {
     public TextField lastNameField;
     public TextField emailField;
     public PasswordField passwordField;
+    public TextField emailFieldLogin;
+    public PasswordField passwordFieldLogin;
+    public Button signInButton;
+    public Button createAccountButton;
 
     public void showAccountCreationDialog(MouseEvent mouseEvent) {
         firstNameField.setDisable(false);
+        lastNameField.setDisable(false);
+        emailField.setDisable(false);
+        passwordField.setDisable(false);
+        createAccountButton.setDisable(false);
+
+        createAccountLink.setUnderline(false);
+        createAccountLink.setTextFill(Color.BLACK);
+    }
+
+    public void signIn(ActionEvent actionEvent) {
+    }
+
+    public void createAccount(ActionEvent actionEvent) {
     }
 }
