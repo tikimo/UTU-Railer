d046f3c469bd96964d1793087a5f37efbaa47c16
tikimo <tikimo@utu.fi>
2018-02-19 12:37:06 +0200

    Portal layout set up

--- src/FrontEnd/Portal/PortalController.java
+++ src/FrontEnd/Portal/PortalController.java
@@ -1,4 +1,17 @@
 package FrontEnd.Portal;
 
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+
 public class PortalController {
+
+    public ScrollPane scrollPane;
+    public AnchorPane scrollPaneAnchorPane;
+
+
+    public void initialize() {
+        // scrollPane.setFitToHeight(true);
+        
+    }
+
 }
