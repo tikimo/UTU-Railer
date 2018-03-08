4e21f0f16ee0d72a3ebf4b6cd74dedeb86576f8b
tikimo <tikimo@utu.fi>
2018-03-08 14:47:13 +0200

    Added design declaration

--- src/FrontEnd/Portal/PortalController.java
+++ src/FrontEnd/Portal/PortalController.java
@@ -1,13 +1,32 @@
 package FrontEnd.Portal;
 
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-
+/**
+ * 1. First the user must select the route
+ *      FROM - TO
+ *      Time of leaving / Arrival time
+ *      Number of passengers
+ *  Program will search the database for matching trains and return them. There must be enough seats!
+ *  Seat configuration and position dont matter when travelling together.
+ *
+ *  2. Second is the position selection
+ *  Seat will have seatType. Types:
+ *      Allergy, Pet, disabled, quiet, family
+ *  Whatever the type, user must be able to select the position to sit in.
+ *
+ *  3. Payment of commute
+ *  User can pay in the application or in the train.
+ *  After payment, program will declare seats taken.
+ *  Upon seats declared taken, other clients will be notified.
+ *
+ *
+ *  X. User settings tab will be activable at any time
+ *  In this tab, user can delcare:
+ *      Address information
+ *      new password
+ *      payment information
+ *
+ */
 public class PortalController {
-
-    public ScrollPane scrollPane;
-    public AnchorPane scrollPaneAnchorPane;
-
 
     public void initialize() {
         // scrollPane.setFitToHeight(true);
